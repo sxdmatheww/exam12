c1697381db19638fae46d45ccacf170e919804c1
Alibekov_MM <Alibekov_MM@mail.ru>
2023-06-22 15:26:15 +0300

    matveymot

---
+++ Desktop/test01.02/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+    public static class OrderAnalyzer
+    {
+        public static List<DateTime> GetPopularMonths(List<DateTime> orderDates)
+        {
+            var groupedDates = orderDates.GroupBy(date => new { date.Year, date.Month });
+
+            var monthlyOrderCounts = groupedDates.Select(group => new { Month = group.Key, Count = group.Count() });
+
+            
+            var sortedMonths = monthlyOrderCounts.OrderByDescending(month => month.Count);
+
+           
+            var result = sortedMonths.Select(month => new DateTime(month.Month.Year, month.Month.Month, 1));
+
+            return result.ToList();
+        }
+    }
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            List<DateTime> orderDates = new List<DateTime>()
+            {
+                new DateTime(2019, 12, 12, 14, 43, 0),
+                new DateTime(2019, 12, 1, 15, 5, 0),
+                new DateTime(2019, 11, 4, 9, 1, 0)
+            };
+
+            List<DateTime> popularMonths = OrderAnalyzer.GetPopularMonths(orderDates);
+
+            Console.WriteLine("Самые популярные месяцы заказов:");
+            foreach (var month in popularMonths)
+            {
+                Console.WriteLine(month.ToString("yyyy-MM-01 00:00"));
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Desktop/test01.02/UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class OrderAnalyzerTests
+        {
+            //1 тест
+            [TestMethod]
+            public void GetPopularMonths_ReturnsCorrectListOfPopularMonths_WhenOrderDatesAreProvided()
+            {
+                List<DateTime> orderDates = new List<DateTime>
+            {
+                new DateTime(2019, 12, 12, 14, 43, 0),
+                new DateTime(2019, 12, 1, 15, 5, 0),
+                new DateTime(2019, 11, 4, 9, 1, 0)
+            };
+
+                List<DateTime> popularMonths = OrderAnalyzer.GetPopularMonths(orderDates);
+
+                Assert.AreEqual(2, popularMonths.Count);
+                Assert.AreEqual(new DateTime(2019, 12, 1), popularMonths[0]);
+                Assert.AreEqual(new DateTime(2019, 11, 1), popularMonths[1]);
+            }
+        }
+        //2 тест
+        [TestMethod]
+        public void GetPopularMonths_ReturnsSingleMonth_WhenOrderDatesContainOnlyOneMonth()
+        {
+            List<DateTime> orderDates = new List<DateTime>
+            {
+                new DateTime(2022, 6, 10, 8, 0, 0),
+                new DateTime(2022, 6, 15, 14, 30, 0),
+                new DateTime(2022, 6, 25, 10, 0, 0)
+            };
+
+            List<DateTime> popularMonths = OrderAnalyzer.GetPopularMonths(orderDates);
+
+            Assert.AreEqual(1, popularMonths.Count);
+            Assert.AreEqual(new DateTime(2022, 6, 1), popularMonths[0]);
+        }
+    }
+
+
+}
